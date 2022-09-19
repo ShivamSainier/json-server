fd9e2649db6ba15dedeb0a3e4a73a05656382f7f
shivam saini <shivamsainier98@gmail.com>
2022-09-19 14:09:11 +0530

    add dialog

--- src/components/dashboard/ContactUs.js
+++ src/components/dashboard/ContactUs.js
@@ -23,25 +23,68 @@ const ContactUs = () => {
     return (
         <>
             <Dialog open={open} onClose={handleClose}>
-                <DialogTitle>Subscribe</DialogTitle>
-                <DialogContent>
-                    <DialogContentText>
-                        To subscribe to this website, please enter your email address here. We
-                        will send updates occasionally.
-                    </DialogContentText>
-                    <TextField
-                        autoFocus
-                        margin="dense"
-                        id="name"
-                        label="Email Address"
-                        type="email"
-                        fullWidth
-                        variant="standard"
-                    />
-                </DialogContent>
+                <DialogTitle sx={{ textAlign: "center", fontSize: { lg: '25px', md: '22px', sm: '18px', xs: '16px' }, fontWeight: "500", color:"#ABD66E"}}>Contact us</DialogTitle>
+                <form>
+                    <DialogContent sx={{ display:'flex',flexDirection:'column',gap:4}}>
+                        
+                        <Box sx={{ display: 'flex', justifyContent: "space-between" }}>
+                            <Box>
+                                <Typography sx={{ fontWeight: "600", color:"#425162"}}>First Name</Typography>
+                                    <TextField
+                                    autoFocus
+                                    margin="dense"
+                                    id="Fname"
+                                    
+                                    type="text"
+                                    fullWidth
+                                        variant="outlined"
+                                        sx={{width:"80%"}}
+                                        />
+                                </Box>
+                            <Box>
+                                <Typography sx={{ fontWeight: "600", color: "#425162" }}>Last Name</Typography>
+                               
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            id="Lname"
+                           
+                            type="text"
+                            fullWidth
+                            variant="outlined"
+                            sx={{ width: "80%" }}
+                                />
+                            </Box>
+                        </Box>
+                        <Box>
+                            <Typography sx={{ fontWeight: "600", color: "#425162" }}>E-mail Id</Typography>
+                         <TextField
+                            autoFocus
+                            margin="dense"
+                            id="email"
+                            type="email"
+                            fullWidth
+                            variant="outlined"
+                            />
+                        </Box>
+                        <Box>
+                            <Typography sx={{ fontWeight: "600", color: "#425162" }}>City</Typography>
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                                id="Address"
+                                multiline
+                                rows={4}
+                            type="text"
+                            fullWidth
+                            variant="outlined"
+                            />
+                        </Box>
+                        <Button type="submit" sx={{ backgroundColor:"#1B9ED9",color:"white",p:1,width:{lg:"50%",md:"50%",sm:'80%',xs:"100%",margin:"auto"}}}>Submit</Button>
+                    </DialogContent>
+                </form>
                 <DialogActions>
-                    <Button onClick={handleClose}>Cancel</Button>
-                    <Button onClick={handleClose}>Subscribe</Button>
+                    
                 </DialogActions>
             </Dialog>
         <Box sx={{width:"80%",margin:"auto",}}>
@@ -52,7 +95,7 @@ const ContactUs = () => {
             </Box>
                 <Box>
                     <Box sx={{ width: { lg: "236px", sm: "216px", md: "226px", xs: "100px" }, height: { lg: "50px", md: "40px", xs: "20px", sm: "40px" }, backgroundImage: "linear-gradient(262deg, #aed874, #8ec640)", borderRadius: "40px", display: "flex", justifyContent: "center", alignItems: "center", boxShadow: "6px 10px 30px 0 rgb(0 0 0 / 23%)" }}>
-                            <Typography color="white" sx={{ fontSize: { lg: '21px', md: '10px', sm: '12px', xs: "7px" } }} fontWeight="600" fontFamily="Montserrat" onClick={handleClickOpen}>Contact Us
+                            <Typography color="white" sx={{ fontSize: { lg: '21px', md: '10px', sm: '12px', xs: "7px" } ,cursor:"pointer",'&hover':{backgroundColor:"green"}}} fontWeight="600" fontFamily="Montserrat" onClick={handleClickOpen}>Contact Us
                         </Typography>
                     </Box>
             </Box>
